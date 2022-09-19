c5e5d1aee05fc3981b68cadb759a5eb7890b1f89
Savio Shamoon <savio.shamoon@mrjaffesclass.com>
2022-09-19 11:19:11 -0700

    Initial commit

---
+++ Operators.java
@@ -0,0 +1,14 @@
+public class Operators
+{
+    public static void main(String[] args)
+    {
+        int i = 10;
+        int f =5;
+        f+= i;
+        boolean flag = true;
+        i++;
+        System.out.print(i);
+        System.out.print(f);
+        System.out.print(flag);
+    }
+}
