1127d07e38513aa07bea282416e56d7cbc99c380
Savio Shamoon <savio.shamoon@mrjaffesclass.com>
2022-09-20 11:03:50 -0700

    commit this

--- Operators.java
+++ Operators.java
@@ -10,5 +10,10 @@ public class Operators
         System.out.print(i);
         System.out.print(f);
         System.out.print(flag);
+        String string1 = "I am a string";
+        String string2=string1;
+        string1.equals(string2);
+        String string3 = "I am also a string";
+        boolean stringEquals = string1.equals(string3);
     }
 }
